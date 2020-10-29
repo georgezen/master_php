6b908e72dd323935c72f43849787d368c1e093ae
georgezen <georgeutm2014@gmail.com>
2020-10-28 23:06:03 -0600

    inicio maqueta crud php

---
+++ crud_php/index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Crud de todas las tecnologias</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+    <div class="container">
+        <header class="header">
+            <h1 class="title">Crud con todo junto (html,css,js,php y mysql)</h1>
+        </header>
+
+        <main class="main">
+            <section class="section">
+                <div class="btn-crud">
+                    <input type="button" value="Consultar crud" id="get_datos" class="get_datos">
+                </div>
+
+
+                <div class="tabla">
+                    <table border="1">
+                        <thead>
+                            <tr>
+                                <td>ID:</td>
+                                <td>Nombre:</td>
+                                <td>Apellidos:</td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>hola</td>
+                                <td>hola2</td>
+                                <td>hola3</td>
+                            </tr>
+
+                        </tbody>
+                    </table>
+                </div>
+
+            </section>
+            <aside class="aside">
+                <h2 class="title-aside">La tabla del lado izquierdo</h2>
+            </aside>
+
+        </main>
+
+        <footer class="footer">
+            <div class="creditos">
+                Derechos reservados Dracosoftware-zen.com
+            </div>
+
+        </footer>
+
+    </div>
+
+<script src="js/app.js"></script>
+</body>
+
+</html>

b8d47e577c303e5d6a3c0c16a555dcea523eaf3d
georgezen <georgeutm2014@gmail.com>
2021-06-10 01:09:12 -0500

    adicion proyecto maqueta repaso despues de gobierno

---
+++ maqueta_regreso/controller.php
@@ -0,0 +1,14 @@
+<?php
+require_once "config_log.php";
+
+$arra = array('gola'=>3,false=>2);
+
+foreach ($arra as $a) {
+    echo $a;
+}
+
+
+
+
+
+
---
+++ maqueta_regreso/index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Maqueta de regreso</title>
+    <link rel="stylesheet" href="assets/index.css">
+</head>
+
+<body>
+
+    <div class="container">
+        <header class="header">
+            <div class="img">
+                <div class="cont_img">
+                    <img src="assets/logo2.png" alt="la putita" title="la">
+                </div>
+            </div>
+            <nav class="navbar">
+                <ul>
+                    <li><a href="">Inicio</a></li>
+                    <li><a href="">Otros</a></li>
+                    <li><a href="">Algunos</a></li>
+                </ul>
+            </nav>
+        </header>
+
+        <section class="section">
+            <div class="card_container">
+                <div class="card">
+                    <article class="article">
+                        <span>Titular</span>
+                        <strong>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod quibusdam molestiae sequi reprehenderit possimus, eligendi eaque error harum officiis cupiditate rerum quo fuga odit asperiores deserunt nulla. Laudantium, cupiditate incidunt.</strong>
+                    </article>
+                </div>
+                <div class="card">
+                    <article class="article">
+                        <span>Titular</span>
+                        <strong>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod quibusdam molestiae sequi reprehenderit possimus, eligendi eaque error harum officiis cupiditate rerum quo fuga odit asperiores deserunt nulla. Laudantium, cupiditate incidunt.</strong>
+                    </article>
+                </div>
+                <div class="card">
+                    <article class="article">
+                        <span>Titular</span>
+                        <strong>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod quibusdam molestiae sequi reprehenderit possimus, eligendi eaque error harum officiis cupiditate rerum quo fuga odit asperiores deserunt nulla. Laudantium, cupiditate incidunt.</strong>
+                    </article>
+                </div>
+                <div class="card">
+                    <article class="article">
+                        <span>Titular</span>
+                        <strong>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod quibusdam molestiae sequi reprehenderit possimus, eligendi eaque error harum officiis cupiditate rerum quo fuga odit asperiores deserunt nulla. Laudantium, cupiditate incidunt.</strong>
+                    </article>
+
+                </div>
+            </div>
+        </section>
+        <aside class="aside">
+            <form action="" id="form1">
+                <label for="">Nombre</label>
+                <input type="text" name="" id="" class="input">
+
+                <label for="">Apellidos</label>
+                <input type="text" name="" class="input">
+
+                <input type="button" value="Guardar">
+            </form>
+        </aside>
+        <footer class="footer">
+            <div class="cont_info">
+                <span>Derechos reservados dracosoftware-zen.com</span>
+            </div>
+        </footer>
+    </div>
+
+
+
+    <script src="assets/index.js"></script>
+</body>
+
+</html>

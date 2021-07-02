636e8aa6c877e0a6d9ed7a434e4b2ed42d031342
georgezen <georgeutm2014@gmail.com>
2021-07-02 14:29:17 -0500

    dsdsds

--- maqueta_regreso/index.php
+++ maqueta_regreso/index.php
@@ -84,7 +84,7 @@
        
         <footer class="footer">
             <div class="cont_info_f">
-                <span>Derechos reservados dracosoftware-zen.com hola pinche putita</span>
+                <span>Derechos reservados dracosoftware-zen.com hola pinche putita,dsdsdsds</span>
             </div>
         </footer>
     </div>
